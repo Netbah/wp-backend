e759cdc40023ff8f57ea37aa80aa70ba5087d261
Sergei Bakhtoiarov <sergey.bakhtoyarov@gmail.com>
2019-08-11 00:36:09 +0200

    controller update

--- src/api/product/product.controller.ts
+++ src/api/product/product.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Post, Req, Body } from '@nestjs/common';
 import { WoocommerceService } from '../../services/woocommerce/woocommerce.service';
 import { ProductDto } from '../../models/ProductDto';
+import { Request } from 'express';
 
-@Controller('product')
+@Controller('api/product')
 export class ProductController {
 
     constructor(private readonly wooService: WoocommerceService) { }
 
     @Post()
-    async create(@Body() product: ProductDto) {
+    async create(@Req() request: Request, @Body() product: ProductDto) {
         const productResponce = await this.wooService.createProduct(product);
         return productResponce;
     }
